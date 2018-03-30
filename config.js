482eb6d5604ee6a1a74befcd2f87904bb6e28611
nidhi1996 <nidhisoni1796@gmail.com>
2018-03-30 19:21:29 +0530

    init

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: '0bRsuHEqhdm9kLJZWafknEhtu',  
+  consumer_secret: 'UKeHMi2CMcEFAjcCIAz67Lk8A75KcnI6tqglL5Rw1B6scIyQhN',
+  access_token: '2611622749-eVEFrQJ3LzLgO4IVxuF9E1ew6hI5Xiec5VnSsC',  
+  access_token_secret: 'EF2aUPo9LCw426ewRNsYtqB9k61rvdKZZf51qtiQPqXt7'
+}
